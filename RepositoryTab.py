9711494dea57d6b018f5b3c38e1a655c9b7e8e48
chnyda <chnyda@mirantis.com>
2018-01-18 15:36:44 +0100

    Add repo

--- RepositoryTab.py
+++ RepositoryTab.py
@@ -18,105 +18,88 @@ class RepositoryTab(QWidget):
         super(RepositoryTab, self).__init__(parent)
 
         # initialize widgets
-        self.test = QTabWidget()
-        self.componentBox = QComboBox()
-        self.publishBox = QComboBox()
-        self.publishButton = QPushButton("Publish")
-        self.reloadButton = QPushButton("Reload")
+        self.repoBox = QComboBox()
+        self.deleteButton = QPushButton("Delete")
+        self.snapshotButton = QPushButton("Snapshot")
+        self.minimalSnapshotButton = QPushButton("Minimal snapshot")
         self.packageLabel = QListView()
         self.packageLabel.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
         # place widget in the layout
         layout = QGridLayout()
-        layout.addWidget(self.publishBox, 0, 0, 1, 1)
-        layout.addWidget(self.componentBox, 0, 1, 1, 1)
-        layout.addWidget(self.publishButton, 0, 2, 1, 1)
+        layout.addWidget(self.repoBox, 0, 0, 1, 1)
+        layout.addWidget(self.deleteButton, 0, 1, 1, 1)
+        layout.addWidget(self.snapshotButton, 0, 2, 1, 1)
+        layout.addWidget(self.minimalSnapshotButton, 1, 2, 1, 1)
         layout.addWidget(self.packageLabel, 1, 1, 2, 1)
-        layout.addWidget(self.reloadButton, 1, 2, 1, 1)
         self.setLayout(layout)
 
         # initialize datas
         self.model = QStandardItemModel(self.packageLabel)
         self.client = self.createClient()
-        self.publishDic = self.preLoadPublishes()
-        self.fillPublishBox()
-        self.recreatePackageBox()
+        self.repoDictionnary = {}
         # controllers
-        self.publishBox.currentIndexChanged.connect(self.updateSnapshotBox)
-        self.componentBox.currentIndexChanged.connect(self.recreatePackageBox)
-        self.publishButton.clicked.connect(self.updatePublish)
-
+        self.loadRepository()
+        self.repoBox.currentIndexChanged.connect(self.updateList)
+        self.deleteButton.clicked.connect(self.removePackage)
 
     def createClient(self):
         return Aptly("http://127.0.0.1:8089", dry=False, timeout=600)
 
-    def preLoadPublishes(self):
-        publishList = {}
-        publishes = self.client.do_get('/publish')
-        for publish in publishes:
-            name = "{}{}{}".format(publish['Storage'] + ":" if publish['Storage']
-                                   else "", publish['Prefix'] + "/" if
-                                   publish['Prefix'] else "",
-                                   publish['Distribution'])
-            publishList[name] = Publish(self.client, name, load=False, storage=publish.get('Storage', "local"))
-        return publishList
+    def loadRepository(self):
+        repos = Publish._get_repositories(self.client)
+        repo_list = []
+        for repo in repos:
+            repo_list.append(repo["Name"])
+        for repo in sorted(repo_list):
+            self.repoBox.addItem(repo)
+        self.repoBox.update()
+        self.updateList()
 
-    def loadSnapshot(self, name):
-        return Publish.get_packages(self.client, "snapshots", name)
+    def updateList(self):
+        self.model.removeRows(0, self.model.rowCount())
+        currentRepo = self.repoBox.currentText()
+        if currentRepo == "":
+            return
+        if currentRepo not in self.repoDictionnary.keys():
+            print(currentRepo)
+            self.repoDictionnary[currentRepo] = sorted(Publish._get_packages(self.client, "repos", currentRepo))
 
-    def updatePublish(self):
-        # check if deep copy of shallow copy
-        currentPublish = self.publishDic[self.publishBox.currentText()]
+        if self.repoDictionnary[currentRepo]:
+            for package in self.repoDictionnary[currentRepo]:
+                item = QStandardItem(package)
+                item.setCheckable(True)
+                item.setCheckState(Qt.Unchecked)
+                self.model.appendRow(item)
+            self.packageLabel.setModel(self.model)
+
+
+    def removePackage(self):
         packageList = []
-        # find a better way to get packages
-        for index in reversed(range(self.model.rowCount())):
+        repoName = self.repoBox.currentText()
+        self.repoDictionnary[repoName] = []
+        for index in range(self.model.rowCount()):
             currentItem = self.model.item(index)
             if currentItem and currentItem.checkState() != 0:
                 packageList.append(currentItem.text())
-                # WE NEED TO RELOAD PUBLISH FOR REAL TO NOT PUBLISH SHIT?
+            else:
+                self.repoDictionnary[repoName].append(currentItem.text())
+        print(packageList)
+        self.client.do_delete('/repos/%s/packages' % repoName, data={'PackageRefs': packageList})
+        self.updateList()
 
-        # TODO: create function for snapshot name?
-        component = self.componentBox.currentText()
-        oldSnapshotName = currentPublish.components[component][0]
-        newSnapshotName = "{}-{}".format(oldSnapshotName, "gui")
-        currentPublish.create_snapshots_from_packages(packageList, newSnapshotName, 'Snapshot created from GUI for component {}'.format(component))
-        currentPublish.replace_snapshot(component, newSnapshotName)
-        currentPublish.do_publish(recreate=False, merge_snapshots=False)
-        # TODO: check the dictionnary is also updated
+    def createMinimalSnapshot(self):
+        # TODO
+        i = 10
+
+    def createSnapshot(self):
+        # TODO
+        i = 100
+
+    # TODO: disable buttons if no packages...
 
 
-    def fillPublishBox(self):
-        self.publishBox.clear()
-        for publish in sorted(self.publishDic.keys()):
-            self.publishBox.addItem(publish)
-        self.publishBox.update()
-        # update snapshot box
-        self.updateSnapshotBox()
 
-    def updateSnapshotBox(self):
-        name = self.publishBox.currentText()
-        currentPublish = self.publishDic[name]
-        currentPublish.load()
-        self.componentBox.clear()
-        for component in sorted(list(currentPublish.components.keys())):
-            self.componentBox.addItem(component)
-        self.componentBox.update()
 
-    def recreatePackageBox(self):
-        self.model.removeRows(0, self.model.rowCount())
-        component = self.componentBox.currentText()
-        currentPublish = self.publishDic[self.publishBox.currentText()]
 
-        # empty sometimes?
-        if not component:
-            return
-
-        packages = sorted(currentPublish._get_packages(self.client, "snapshots", currentPublish.components[component][0]))
-
-        for package in packages:
-            item = QStandardItem(package)
-            item.setCheckable(True)
-            item.setCheckState(Qt.Checked)
-            self.model.appendRow(item)
-        self.packageLabel.setModel(self.model)
 
