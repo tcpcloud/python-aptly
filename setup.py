169d3e924f7b203d7497cf9448186ec692caf0b3
Filip Pytloun <filip@pytloun.cz>
2018-01-30 21:27:38 +0100

    Version 0.12.11

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_desc = open('README.rst').read()
 
 setup(
     name="python-aptly",
-    version="0.12.10",
+    version="0.12.11",
     description="Aptly REST API client and tooling",
     long_description=long_desc,
     author="Filip Pytloun",
